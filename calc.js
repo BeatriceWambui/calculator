67a1be0224b5e8252b85279b6cc8b672c98c2372
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-07-10 17:31:04 +0300

    add initial commits

---
+++ calc.js
@@ -0,0 +1,21 @@
+
+var add = function (number1, number2){ return number1 + number2};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2=parseInt(prompt("Enter another number:"));
+var result=add(number1,number2);
+
+var subtract = function (number1, number2){
+  return number1 - number2;
+};
+var differrence=subtract(number1, number2);
+
+var multiplication=function(number1,number2){
+  return number1*number2
+};
+var multiply=multiplication(number1,number2);
+
+var divition=function (number1, number2){
+  return number1/number2;
+};
+var divide =divition(number1,number2);
+alert(divide);
